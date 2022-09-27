6c0e3ed8da8cd32607357f2fd50c04bcc901ab3a
bhanu <udaibhanu2602@gmail.com>
2022-09-27 14:28:14 +0530

    Debounding task-8

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+import { cleanup } from "@testing-library/react";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-useEffect(()=>{setFormIsValid(
-  enteredEmail.includes('@') && enteredPassword.trim().length > 6
-);},[enteredEmail,enteredPassword])
+
+  useEffect(()=>
+  {
+    console.log('hlo');
+    return()=>{console.log('hi')}
+  },[])
+
+  useEffect(() => {
+    const setTimeoutID = setTimeout(() => {
+      console.log("checking validity");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log('cleanup')
+      clearTimeout(setTimeoutID);
+    };
+  }, [enteredEmail, enteredPassword]);
+
+  //here we are using debounding, means not checking function on every input but after when user pause entering keys
+  //we are puttting setFormIsValid state in setTimeout, and will executed after 500 ms
+  //and using return in first input , which will not executed initially but executed first then setTimeout  after that
+  //here initially setTimeout, will executed first and then return will proceed, but after that return will executed first
+  //therefore we are cleaning setTimeoutID after input and hence only last setTimeout, will proceeded
+  //This debounding is helpful in seding http requests, if you send request on every input, code will slow down , therefor now http request will only send after user enter input and then pause
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
@@ -24,7 +48,7 @@ useEffect(()=>{setFormIsValid(
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -41,7 +65,7 @@ useEffect(()=>{setFormIsValid(
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -55,7 +79,7 @@ useEffect(()=>{setFormIsValid(
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
